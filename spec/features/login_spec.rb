13830004a1e8e91e7f70243a4c59e72061aefdf7
mtbtiago <tiago.ameller@gmail.com>
2015-04-04 21:36:08 +0200

    add logout test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'rails_helper'
 require 'ap'
 
-describe "The login process", :type => :feature do
+describe "The login/logout process", :type => :feature do
   before do
     @issuer = FactoryGirl.build(:issuer,email: "B57534125@GMAIL.COM", password: "12345")
     @issuer.save!
@@ -25,11 +25,24 @@ describe "The login process", :type => :feature do
       expect(page).to have_content "Invalid email/password combination"
     end
 
-    def try_login_with(email, password)
-      fill_in 'Email', with: email
-      fill_in 'Password', with: password
+  end
 
-      click_button 'Log in'
+  feature 'Logout' do
+
+    scenario 'user clicks logout' do
+      visit login_path
+      try_login_with(@issuer.email,"12345")
+      expect(page).to have_content @issuer.company_name
+
+      click_link 'Log out'
+      expect(page).to have_content "Remember me on this computer for 15 days"
     end
+  end
+
+  def try_login_with(email, password)
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+
+    click_button 'Log in'
   end
 end
